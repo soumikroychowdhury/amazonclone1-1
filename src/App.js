c060d35ea678559bb3d34c5c2fec291f07617231
soumikroychowdhury <eventssoumik01@gmail.com>
2023-05-14 23:47:10 +0530

    amazon clone till basic checkout

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Header from './Header'
+import Home from './Home'
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+import Checkout from './Checkout'
+
+function App() {
+  return (
+    <Router>
+    <div className="app">
+    <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/checkout" element={<Checkout/>}/>
+      </Routes>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
