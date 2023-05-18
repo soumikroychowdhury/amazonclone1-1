9bbb088bbdaceedadd03f098305784a6a10a0be9
soumikroychowdhury <eventssoumik01@gmail.com>
2023-05-18 12:48:08 +0530

    amazone clone till user authentication

--- src/App.js
+++ src/App.js
@@ -3,15 +3,36 @@ import Header from './Header'
 import Home from './Home'
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 import Checkout from './Checkout'
+import Login from './Login'
+import {useEffect} from "react";
+import {auth,db} from './firebase'
+import {useStateValue} from './StateProvider'
 
 function App() {
+  const [{},dispatch]=useStateValue();
+  useEffect(()=>{
+    auth.onAuthStateChanged(authUser=>{
+      console.log("The user is ",authUser);
+      if(authUser){
+        dispatch({
+          type:"SET_USER",
+          user:authUser
+        })
+      }else{
+        dispatch({
+          type:"SET_USER",
+          user:null
+        })
+      }
+    })
+  },[])
   return (
     <Router>
     <div className="app">
-    <Header/>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/checkout" element={<Checkout/>}/>
+        <Route path="/" element={[<Header/>,<Home/>]}/>
+        <Route path="/checkout" element={[<Header/>,<Checkout/>]}/>
+        <Route path="/login" element={<Login/>}/>
       </Routes>
     </div>
     </Router>
--- src/reducer.js
+++ src/reducer.js
@@ -26,7 +26,9 @@ const reducer = (state, action) => {
       }else{
         console.warn(`Can't remove product (id: ${action.id}) as its not in basket!`);
       }
-      return {...state,basket:newBasket}
+      return {...state,basket:newBasket};
+    case "SET_USER":
+      return {...state,user:action.user};
     default:
       return state;
   }
